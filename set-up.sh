28237e64fafea46be5a95b557c4ffd16324d9752
leizh <leizhnxp@gmail.com>
2021-03-24 17:48:00 +0800

    应用缺省的ZSH Custom目录
    + 其实正主是PATH复写
    + 之前LANG有设错
    + git log还要再加一个配置？

--- set-up.sh
+++ set-up.sh
@@ -35,7 +35,7 @@ git config --global core.editor vim
 
 # setup my custom zsh content
 
-[[ -d $ZSH_CUSTOM ]] && ln -sf $provision_dir/dotfiles/myzshrc.zsh $ZSH_CUSTOM/myzshrc.zsh 
+[[ -d $ZSH_CUSTOM ]] && ln -sf $provision_dir/dotfiles/myzshrc.zsh $ZSH_CUSTOM/myzshrc.zsh || ln -sf $provision_dir/dotfiles/myzshrc.zsh $ZSH/custom/myzshrc.zsh
 
 # only for bash
 #curl -L https://raw.githubusercontent.com/juven/maven-bash-completion/master/bash_completion.bash -o ~/.maven_bash_completion.bash
